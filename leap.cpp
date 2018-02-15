b1466cab2486efb06d7da5a4440524d01a237957
surega <36365913+surega@users.noreply.github.com>
2018-02-15 10:03:03 +0530

    Create leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter the value n";
+    cin>>n;
+    if(n%4==0)
+    cout<<"leap year";
+    else
+    cout<<"not leap year";
+}
