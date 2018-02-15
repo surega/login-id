5bd799aa39978fb4adac2505474a734899dda273
surega <36365913+surega@users.noreply.github.com>
2018-02-15 14:05:26 +0530

    Create oddbetween.cpp

---
+++ oddbetween.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a,b,i;
+	cout<<"enter the values of a,b";
+	cin>>a>>b;
+	for(i=0;a<=b;i++)
+{
+	cout<<a<<endl;
+	a=a+2;
+}
+	return 0;
+}
