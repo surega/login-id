a3a744bc4cfd0d1780c75ff1ff55500e0550bd8a
surega <36365913+surega@users.noreply.github.com>
2018-02-15 12:24:46 +0530

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,s=0,i;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {s++;
+        }
+    }
+    if(s==2)
+    cout<<"prime";
+    else
+    cout<<"not prime";
+}
