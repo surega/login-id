e9e445f5615c63741c66be145c06a5211f7075c5
surega <36365913+surega@users.noreply.github.com>
2018-02-15 10:11:19 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n;
+    cout<<"enter the value of n";
+    cin>>n;
+    for(i=0;i<=n;i++)
+    {
+        cout<<"hello"<<endl;
+    }
+}
