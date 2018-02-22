64dd953d9e649c40967224c80045a788de7891b5
surega <36365913+surega@users.noreply.github.com>
2018-02-22 17:37:06 +0530

    Create 5multiples.cpp

---
+++ 5multiples.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i,n,res=0;
+	cout<<"enter the value of n";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+             cout<<"5*n"<<i<<5*i<<endl; 
+	}
+}
