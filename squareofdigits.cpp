5dcb5e347a6adca72fe066c82431ca05a59eb3b8
surega <36365913+surega@users.noreply.github.com>
2018-02-27 17:40:07 +0530

    Create squareofdigits.cpp

---
+++ squareofdigits.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+int main()
+{
+ int n,a=0,r,q;
+cout<<"Enter the number to be tested: ";
+cin>>n;
+ q=n;
+ do
+ {
+      r=q%10;
+      a=a+pow(r,2);
+      q=q/10;
+ }
+ while(q%10!=0);
+ cout<<a;
+
+}
