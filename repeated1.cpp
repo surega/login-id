cff806e7194e96a6c37ee8a1fff627c5a1541469
surega <36365913+surega@users.noreply.github.com>
2018-03-07 17:05:46 +0530

    Create repeated1.cpp

---
+++ repeated1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+  int i,arr[20],j,n;
+  cout<<"Enter Size of array: ";
+  cin>>n;
+  cout<<"Enter any "<<n<<" num in array: ";
+  for(i=0;i<n;i++)
+  {
+   cin>>arr[i];
+  }
+  cout<<"Repeated Values are: ";
+  for(i=0; i<n; i++)
+   {
+    for(j=i+1;j<n;j++)
+    {
+    if(arr[i]==arr[j])
+    {
+    cout<<"\n"<<arr[i];
+    }
+   }
+   }
+   return 0;
+ }
