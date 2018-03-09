22fdfddc1dc4cf69bb04fa36fdbb8e2113624e04
surega <36365913+surega@users.noreply.github.com>
2018-03-09 17:45:21 +0530

    Create dividearray.cpp

---
+++ dividearray.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+ int arr[30];
+ int i, n, a, j;
+ cin>>n;
+ for (i = 0; i < n; ++i)
+    cin>>arr[i]);
+    cin>>a;
+ for (i = 0; i < a; ++i)
+ {
+   arr[n] = arr[0];
+   for (j = 0; j < n; ++j) 
+    number[j] = number[j + 1];
+ }
+
+for (i = 0; i < n; ++i)
+    cout<<arr[i];
+}
