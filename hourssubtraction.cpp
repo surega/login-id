f1c61f8a2dd08ce659352b3710e578ef8f0cf24b
surega <36365913+surega@users.noreply.github.com>
2018-05-23 11:45:08 +0530

    Create hourssubtraction.cpp

---
+++ hourssubtraction.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int hr1,min1,hr2,min2,hr,min;
+		cin>>hr1>>min1>>hr2>>min2;
+	hr=hr1-hr2;
+	min=min1-min2;
+		cout<<hr<<"hr"<<min<<"min";
+	return 0;
+}
