1a09f438d7ca91338c090b05114ca9a352e48494
surega <36365913+surega@users.noreply.github.com>
2018-05-24 10:11:15 +0530

    Create greatestof10.cpp

---
+++ greatestof10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10],i,max;
+    for(i=0;i<10;i++)
+    {
+        cin>>a[i];
+    }
+    max=a[0];
+    for(i=0;i<10;i++)
+    {
+        if(a[i]>max)
+        max=a[i];
+    }
+    cout<<max;
+    return 0;
+}
