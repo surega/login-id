06a7ff1ee850d4db89a845f093d3a63dedfdfb40
surega <36365913+surega@users.noreply.github.com>
2018-05-25 15:25:56 +0530

    Create 52.cpp

---
+++ 52.cpp
@@ -0,0 +1,27 @@
+#include<iostream.h>
+#include<conio.h>
+
+void once(int a)
+{
+switch(a)
+{
+case 1: cout<<“One”;
+break;
+case 2: cout<<“Two”;
+break;
+case 3: cout<<“Three”;
+break;
+case 4: cout<<“Four”;
+break;
+case 5: cout<<“Five”;
+break;
+case 6: cout<<“Six”;
+break;
+case 7: cout<<“Seven”;
+break;
+case 8: cout<<“Eight”;
+break;
+case 9: cout<<“Nine”;
+break;
+}
+}
